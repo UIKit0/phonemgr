31efbf75fc64c96f257cb4aad6191422a9f175ff
Bastien Nocera <hadess@hadess.net>
2007-07-11 11:35:40 +0000

    Make it compile again with F7's glib
    2007-07-11  Bastien Nocera  <hadess@hadess.net>
    
    	* src/connection.c: (initialise_connection): Make it compile again
    	with F7's glib
    
    
    svn path=/trunk/; revision=135

--- src/connection.c
+++ src/connection.c
@@ -239,8 +239,14 @@ initialise_connection (MyApp *app)
 			G_CALLBACK (on_message), (gpointer) app);
 	app->battery_cb = g_signal_connect (G_OBJECT (app->listener), "battery",
 						G_CALLBACK (on_battery), (gpointer) app);
+#if GLIB_CHECK_VERSION(2,13,0)
 	app->reconnector = g_timeout_add_seconds (20, (GSourceFunc)attempt_reconnect,
 						  (gpointer)app);
+#else
+	app->reconnector = g_timeout_add (20 * 1000, (GSourceFunc)attempt_reconnect,
+					  (gpointer)app);
+#endif
+
 	gdk_threads_init ();
 	app->connecting_mutex = g_mutex_new ();
 	app->message_mutex = g_mutex_new ();
